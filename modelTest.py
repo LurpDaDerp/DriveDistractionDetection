a71a91c3fbe28d5d98e74fdd0276aedf8dc7f9aa
LurpDaDerp <lurpdaderp@gmail.com>
2025-10-13 23:06:19 -0700

    made prediction/testing script to run model on real data (somewhat successful)

--- modelTest.py
+++ modelTest.py
@@ -1,13 +1,44 @@
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
+import matplotlib.pyplot as plt
 
-model = load_model("notebooks\distracted_driver_acc98.keras") 
+#load model
+model = load_model("notebooks/distracted_driver_detection.keras") 
 
-img = image.load_img("C:/Users/lurpd/Downloads/4.jpeg", target_size=(128,128))
+#make class names (c0–c9 from training data)
+class_names = [
+    "Safe driving",                 
+    "Texting - right hand",          
+    "Talking on phone - right hand", 
+    "Texting - left hand",           
+    "Talking on phone - left hand",  
+    "Operating the radio",           
+    "Drinking",                      
+    "Reaching behind",               
+    "Hair and makeup",               
+    "Talking to passenger"           
+]
+
+#image 
+img_path = "C:/Users/lurpd/Downloads/12.jpg"
+
+#process image
+img = image.load_img(img_path, target_size=(128,128))
 img_array = image.img_to_array(img)
 img_array = np.expand_dims(img_array, axis=0) / 255.0
 
+#predict
 predictions = model.predict(img_array)
-predicted_class = np.argmax(predictions, axis=1)
-print("Predicted class:", predicted_class[0])
+predicted_class = np.argmax(predictions, axis=1)[0]
+predicted_label = class_names[predicted_class]
+
+#print results to terminal
+print("Predicted class index:", predicted_class)
+print("Predicted label:", predicted_label)
+
+#show image and results in window
+plt.imshow(image.load_img(img_path))
+plt.axis("off")
+plt.title(f"Prediction: {predicted_label}")
+plt.show()
