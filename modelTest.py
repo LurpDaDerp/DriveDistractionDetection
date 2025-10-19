017e257763166126464ea53f2399e50db01482eb
LurpDaDerp <lurpdaderp@gmail.com>
2025-10-18 18:38:36 -0700

    manually adjusted class weights for likely/important driving behaviors

--- modelTest.py
+++ modelTest.py
@@ -9,24 +9,32 @@ model = load_model("notebooks/distracted_driver_detection.keras")
 
 #class names
 class_names = [
-    "Safe driving", "Texting - right hand", "Talking on phone - right hand",
-    "Texting - left hand", "Talking on phone - left hand", "Operating the radio",
-    "Drinking", "Reaching behind", "Hair and makeup", "Talking to passenger"
+    "Safe driving", 
+    "Operating the radio",
+    "Drinking", 
+    "Reaching behind", 
+    "Hair and makeup", 
+    "Talking to passenger",
+    "General Distracted",
+    "Sleepy",
+    "Yawn"
 ]
 
-#load image
-img_path = "C:/Users/lurpd/Documents/Development/Datasets/MyData/28.jpg"
-
+#load and preprocess image
+img_path = "C:/Users/lurpd/Documents/Development/Datasets/MyData/49.png"
 target_size = (128, 128)
-img = Image.open(img_path).convert("L") 
 
-img = ImageOps.pad(img, target_size, color=(0, 0, 0), method=Image.Resampling.LANCZOS)
+#open image in grayscale
+img = Image.open(img_path).convert("L")
 
-#normalize
-img_array = image.img_to_array(img)
-img_array = np.expand_dims(img_array, axis=0) / 255.0
+#letterbox
+img = ImageOps.pad(img, target_size, color=0, method=Image.Resampling.LANCZOS)
 
-#run model
+#convert to NumPy and normalize
+img_array = np.array(img).astype(np.float32) / 255.0
+img_array = np.expand_dims(img_array, axis=(0, -1))  
+
+#prediction
 predictions = model.predict(img_array)
 predicted_class = np.argmax(predictions, axis=1)[0]
 predicted_label = class_names[predicted_class]
@@ -35,8 +43,8 @@ predicted_label = class_names[predicted_class]
 print("Predicted class index:", predicted_class)
 print("Predicted label:", predicted_label)
 
-#image display
-plt.imshow(img)
+#display for debug
+plt.imshow(img, cmap='gray')
 plt.axis("off")
 plt.title(f"Prediction: {predicted_label}")
 plt.show()
